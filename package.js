e0cc9d99099ca14c5688dcb2ac61974812e8365f
Christian Storch <ccstorch@gmail.com>
2016-05-13 17:33:12 +0100

    0.1.0 version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cinn:publication-builder',
-  version: '0.0.2',
+  version: '0.1.0',
   summary: 'Simple query generator for meteor apps',
   git: 'https://github.com/cinn-labs/publication-builder',
   documentation: 'README.md'
